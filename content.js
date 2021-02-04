c85e5832b78e1780bf29c14dc86a1c08bffd7b62
Glauber Ramos <glauberamos@gmail.com>
2021-02-04 20:22:09 -0300

    Allow linkedin updates

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ css.innerHTML = `
   [aria-label='Timeline: Trending now'], /* twitter trends */
   [aria-label='Who to follow'],          /* twitter who to follow */
   .right-rail,                           /* linkedin sidebar */
-  .core-rail,                            /* linkedin main timeline */
+  .core-rail:not(.update-outlet),        /* linkedin main timeline */
   .ad-banner-container,                  /* linkedin ads */ 
   [title='Trending'],                    /* youtube trending button */
   #secondary,                            /* youtube sidebar recommended videos */
