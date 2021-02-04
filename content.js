163af514e046e0cebb4f29dc7b49f129ef71d7b6
Glauber Ramos <glauberamos@gmail.com>
2021-02-04 08:38:00 -0300

    Improve readability

--- content.js
+++ content.js
@@ -1,16 +1,14 @@
 const css = document.createElement("style");
 
-const youtubeTrendingButton = `[title='Trending']`;
-
 css.innerHTML = `
-  [title='Trending'], /* youtube trending button */
-  [data-testid='trend'], /* twitter trends */
+  [data-testid='trend'],    /* twitter trends */
   [data-testid='UserCell'], /* twitter who to follow */
-  .feed-follows-module, /* linkedin add to your feed */
-  .learning-top-courses, /* linkedin learning top courses */
-  .core-rail, /* linkedin main timeline */
-  #secondary, /* youtube sidebar recommended videos */
-  [page-subtype='home'] { /* youtube home timeline */
+  .feed-follows-module,     /* linkedin add to your feed */
+  .learning-top-courses,    /* linkedin learning top courses */
+  .core-rail,               /* linkedin main timeline */
+  [title='Trending'],       /* youtube trending button */
+  #secondary,               /* youtube sidebar recommended videos */
+  [page-subtype='home'] {   /* youtube home timeline */
     display: none !important;
   }
 `;
