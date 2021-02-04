284c22733c9ddf9897d98fe26d6f2b81f191a2e3
Glauber Ramos <glauberamos@gmail.com>
2021-02-04 08:55:05 -0300

    Improved twitter selectors

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 const css = document.createElement("style");
 
 css.innerHTML = `
-  [data-testid='trend'],    /* twitter trends */
-  [data-testid='UserCell'], /* twitter who to follow */
-  .right-rail,              /* linkedin sidebar */
-  .feed-shared-update-v2,   /* linkedin main timeline posts */
-  .ad-banner-container,     /* linkedin adds */ 
-  [title='Trending'],       /* youtube trending button */
-  #secondary,               /* youtube sidebar recommended videos */
-  [page-subtype='home'] {   /* youtube home timeline */
+  [aria-label='Timeline: Trending now'], /* twitter trends */
+  [aria-label='Who to follow'],          /* twitter who to follow */
+  .right-rail,                           /* linkedin sidebar */
+  .feed-shared-update-v2,                /* linkedin main timeline posts */
+  .ad-banner-container,                  /* linkedin adds */ 
+  [title='Trending'],                    /* youtube trending button */
+  #secondary,                            /* youtube sidebar recommended videos */
+  [page-subtype='home'] {                /* youtube home timeline */
     display: none !important;
   }
 `;
