09ae73b8450c6f25c4d1409fae4af7a09521efb8
Glauber Ramos <glauberamos@gmail.com>
2021-02-04 08:44:26 -0300

    Linkedin improvements

--- content.js
+++ content.js
@@ -3,9 +3,9 @@ const css = document.createElement("style");
 css.innerHTML = `
   [data-testid='trend'],    /* twitter trends */
   [data-testid='UserCell'], /* twitter who to follow */
-  .feed-follows-module,     /* linkedin add to your feed */
-  .learning-top-courses,    /* linkedin learning top courses */
-  .core-rail,               /* linkedin main timeline */
+  .right-rail,              /* linkedin sidebar */
+  .feed-shared-update-v2,   /* linkedin main timeline posts */
+  .ad-banner-container,     /* linkedin adds */ 
   [title='Trending'],       /* youtube trending button */
   #secondary,               /* youtube sidebar recommended videos */
   [page-subtype='home'] {   /* youtube home timeline */
@@ -14,5 +14,4 @@ css.innerHTML = `
 `;
 
 css.type = "text/css";
-
 document.documentElement.appendChild(css);
