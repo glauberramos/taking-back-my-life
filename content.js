e04c6c6e50eeee627e9663d9855b560700f777ef
Glauber Ramos <glauberamos@gmail.com>
2021-02-27 23:44:24 -0300

    personal updates

--- content.js
+++ content.js
@@ -3,16 +3,17 @@ const css = document.createElement("style");
 css.innerHTML = `
   [aria-label='Timeline: Trending now'], /* twitter trends */
   [aria-label='Who to follow'],          /* twitter who to follow */
-  .right-rail,                           /* linkedin sidebar */
-  .core-rail:not(.update-outlet),        /* linkedin main timeline */
+  .scaffold-layout__aside,               /* linkedin sidebar */
   .ad-banner-container,                  /* linkedin ads */ 
   [title='Trending'],                    /* youtube trending button */
   #secondary,                            /* youtube sidebar recommended videos */
-  .ytp-suggestion-set,                   /* youtube suggestions */
-  [page-subtype='home'] {                /* youtube home timeline */
+  .ytp-suggestion-set {                  /* youtube suggestions */
     display: none !important;
   }
 `;
 
 css.type = "text/css";
 document.documentElement.appendChild(css);
+
+//.core-rail:not(.update-outlet),        /* linkedin main timeline */
+//[page-subtype='home'],                 /* youtube home timeline */
